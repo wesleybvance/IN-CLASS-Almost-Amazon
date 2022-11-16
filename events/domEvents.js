22a18efbb2f29671a97e9933cc29cf39399cc21a
Wesley Vance <wslyvnce@gmail.com>
2022-11-15 19:28:41 -0600

    updating classwork to main

--- events/domEvents.js
+++ events/domEvents.js
@@ -1,11 +1,21 @@
+import { deleteSingleAuthor, getAuthors } from '../api/authorData';
+import { deleteBook, getBooks } from '../api/bookData';
+import { showAuthors } from '../pages/authors';
+import { showBooks } from '../pages/books';
+
 const domEvents = () => {
   document.querySelector('#main-container').addEventListener('click', (e) => {
     // TODO: CLICK EVENT FOR DELETING A BOOK
     if (e.target.id.includes('delete-book')) {
       // eslint-disable-next-line no-alert
       if (window.confirm('Want to delete?')) {
-        console.warn('CLICKED DELETE BOOK', e.target.id);
-        console.warn(e.target.id.split('--'));
+        // console.warn('CLICKED DELETE BOOK', e.target.id);
+        // console.warn(e.target.id.split('--'));
+        const [, firebaseKey] = e.target.id.split('--');
+
+        deleteBook(firebaseKey).then(() => {
+          getBooks().then(showBooks);
+        });
       }
     }
 
@@ -29,8 +39,13 @@ const domEvents = () => {
     if (e.target.id.includes('delete-author-btn')) {
       // eslint-disable-next-line no-alert
       if (window.confirm('Want to delete?')) {
-        console.warn('DELETE AUTHOR', e.target.id);
-        console.warn(e.target.id.split('--'));
+        // console.warn('DELETE AUTHOR', e.target.id);
+        // console.warn(e.target.id.split('--'));
+        const [, fireBaseKey] = e.target.id.split('--');
+
+        deleteSingleAuthor(fireBaseKey).then(() => {
+          getAuthors().then(showAuthors);
+        });
       }
     }
 
--- events/navigationEvents.js
+++ events/navigationEvents.js
@@ -1,4 +1,8 @@
+import { getAuthors } from '../api/authorData';
+import { booksOnSale, getBooks } from '../api/bookData';
+import { showBooks } from '../pages/books';
 import { signOut } from '../utils/auth';
+import { showAuthors } from '../pages/authors';
 
 // navigation events
 const navigationEvents = () => {
@@ -8,12 +12,12 @@ const navigationEvents = () => {
 
   // TODO: BOOKS ON SALE
   document.querySelector('#sale-books').addEventListener('click', () => {
-    console.warn('CLICKED SALE BOOKS');
+    booksOnSale().then(showBooks);
   });
 
   // TODO: ALL BOOKS
   document.querySelector('#all-books').addEventListener('click', () => {
-    console.warn('CLICKED ALL BOOKS');
+    getBooks().then(showBooks);
   });
 
   // FIXME: STUDENTS Create an event listener for the Authors
@@ -21,7 +25,7 @@ const navigationEvents = () => {
   // 2. Convert the response to an array because that is what the makeAuthors function is expecting
   // 3. If the array is empty because there are no authors, make sure to use the emptyAuthor function
   document.querySelector('#authors').addEventListener('click', () => {
-    console.warn('CLICKED AUTHORS');
+    getAuthors().then(showAuthors);
   });
 
   // STRETCH: SEARCH
